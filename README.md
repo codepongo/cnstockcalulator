c50861c2480d5122c4bc722c4ca9c81911da861b
unknown <zhuhuotui@gmail.com>
2015-12-09 08:02:54 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# cnstockcalulator
+CNStockCalculator
+=========================
+CNStockCalculator is an iOS Application calculates the break even price of the purchase and the profit or loss of the trade for China stock market participators.
---
+++ source/StockCalculator/StockCalculator/SettingController.h
@@ -0,0 +1,16 @@
+//
+//  SettingController.h
+//  StockCalculator
+//
+//  Created by zuohaitao on 15/12/5.
+//  Copyright © 2015年 zuohaitao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingController : UITableViewController
+@property IBOutlet UILabel* commission;
+@property IBOutlet UILabel* stamp;
+@property IBOutlet UILabel* transfer;
+@property IBOutlet UILabel* version;
+@end
